995cca0d820c2444bcae1e88fc735aac9066c94b
贺志营 <l>
2020-11-24 20:05:31 +0800

    add springboot-actuator

---
+++ springboot-11-actuator/src/main/java/com/hzy/Springboot11ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.hzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot11ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot11ActuatorApplication.class, args);
+    }
+
+}
