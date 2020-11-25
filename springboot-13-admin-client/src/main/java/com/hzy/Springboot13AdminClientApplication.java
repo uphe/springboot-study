60f37650287a7ede9c2a5bb3e6e6a7c52e6a96d2
贺志营 <l>
2020-11-25 15:25:51 +0800

    springboot-admin

---
+++ springboot-13-admin-client/src/main/java/com/hzy/Springboot13AdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.hzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot13AdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot13AdminClientApplication.class, args);
+    }
+
+}
