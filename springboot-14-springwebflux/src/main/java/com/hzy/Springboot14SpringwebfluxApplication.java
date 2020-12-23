f300e8d9635d34ae84cb8905b56c06e81ed578f0
贺志营 <l>
2020-12-23 10:51:10 +0800

    add webflux

---
+++ springboot-14-springwebflux/src/main/java/com/hzy/Springboot14SpringwebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.hzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot14SpringwebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot14SpringwebfluxApplication.class, args);
+    }
+
+}
---
+++ springboot-14-springwebflux/src/main/java/com/hzy/controller/MyController.java
@@ -0,0 +1,73 @@
+package com.hzy.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+@Slf4j
+@RestController
+public class MyController {
+
+    /**
+     * 传统的 spring mvc 开发方式
+     */
+    @GetMapping("/mvc")
+    public String mvc() {
+        long timeMillis = System.currentTimeMillis();
+        log.info("mvc() start");
+        String result = createStr();
+        log.info("mvc() end use time {}/ms", System.currentTimeMillis() - timeMillis);
+
+        return result;
+    }
+
+    /**
+     * spring webflux 的开发方式
+     */
+    @GetMapping("/mono")
+    public Mono<String> mono() {
+        long timeMillis = System.currentTimeMillis();
+        log.info("webflux() start");
+        Mono<String> result = Mono.fromSupplier(this::createStr);
+        log.info("webflux() end use time {}/ms", System.currentTimeMillis() - timeMillis);
+
+        return result;
+    }
+
+    /**
+     * 使用flux，像流一样返回0-N个元素
+     */
+    @GetMapping(value = "/flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> flux() {
+        long timeMillis = System.currentTimeMillis();
+        log.info("webflux() start");
+        Flux<String> result = Flux.fromStream(IntStream.range(1, 5).mapToObj(i -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "flux data--" + i;
+        }));
+        log.info("webflux() end use time {}/ms", System.currentTimeMillis() - timeMillis);
+
+        return result;
+    }
+
+    private String createStr() {
+        // 模拟耗时操作
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "some string";
+    }
+}
